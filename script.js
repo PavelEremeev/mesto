b8be0a3ad5524a80c8087735a36ae2599ca547bf
PavelEremeev <pe19950520@yandex.ru>
2020-05-01 20:40:00 +0300

    js: making cards by template

--- script.js
+++ script.js
@@ -1,30 +1,87 @@
 // Задаем константы
 
-const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
-const profile = document.querySelector('.profile');
+const addButton = document.querySelector('.profile__add-button');
+const titleProfile = document.querySelector('.popup__text');
 const nameProfile = document.querySelector('.profile__text');
 const jobProfile = document.querySelector('.profile__subtext');
 const nameInput = document.querySelector('.popup__input_name');
 const jobInput = document.querySelector('.popup__input_job');
 const formElement = document.querySelector('.popup__form');
 
-// Открытие поп-ап
+const cards = [
+    {
+        name: 'Франция',
+        link: 'https://images.unsplash.com/photo-1562815243-3e7c83b4b4a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+        name: 'Сидней',
+        link: 'https://images.unsplash.com/photo-1525755596908-8f1446d13044?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80'
+    },
+    {
+        name: 'Тула',
+        link: 'https://versiya.info/uploads/posts/2017-12/1512743766_40.jpg'
+    },
+    {
+        name: 'Москва',
+        link: 'https://images.unsplash.com/photo-1512495039889-52a3b799c9bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+    },
+    {
+        name: 'Бали',
+        link: 'https://images.unsplash.com/photo-1537953773345-d172ccf13cf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+// Добавление карточек
+function makeCards(item) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const elements = document.querySelector('.elements');
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector('.element__text').textContent = item.name;
+    cardElement.querySelector('.element__image').src = item.link;
+
+    elements.append(cardElement);
+};
+
+cards.forEach(makeCards);
+
+
+// Открытие поп-ап редактирования профиля
 
 function popupOpen() {
-        popup.classList.add('popup_opened');
+        popupEdit.classList.add('popup_opened');
+
         nameInput.value = nameProfile.textContent; // данные строки отвечают за установку исходных name/job в  форму input
         jobInput.value = jobProfile.textContent;
-}
+};
 
-// Закрытие поп-ап
+
+
+// Закрытие поп-ап редактирования профиля
 
 function popupClose() {
-    popup.classList.remove('popup_opened');
+    popupEdit.classList.remove('popup_opened');
 
 
-}
+};
+
+// Открытие поп-ап нового места
+
+function popupOpenNewPlace() {
+    popupEdit.classList.add('popup_opened');
+
+    titleProfile.textContent = 'Новое место';
+    nameInput.textContent = 'Название';
+    jobInput.textContent = 'Ccылка на картинку';
+};
+
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -44,6 +101,8 @@ function formSubmitHandler (evt) {
     nameProfile.textContent = nameInput.value; 
     jobProfile.textContent = jobInput.value;
 
+
+
     popupClose();
 }
 
@@ -55,3 +114,4 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 editButton.addEventListener ('click', popupOpen);
 closeButton.addEventListener ('click', popupClose);
+addButton.addEventListener ('click', popupOpenNewPlace);
