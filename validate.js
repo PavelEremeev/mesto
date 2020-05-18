7b3cee9165fd98ad4edc8105e9118f7697169a47
PavelEremeev <pe19950520@yandex.ru>
2020-05-18 21:41:07 +0300

    js: write validation

--- validate.js
+++ validate.js
@@ -1,8 +1,10 @@
-const errorElement = formElement.querySelector(`#${inputElement.id}-error`)
+
+
 
 
 // Добавляем класс ошибки элементу input
 const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
     inputElement.classList.add('popup__input_type_error');
     errorElement.textContent = errorMessage;
     errorElement.classList.add('popup__input-error_active');
@@ -10,10 +12,13 @@ const showInputError = (formElement, inputElement, errorMessage) => {
 
 // Удаляем класс ошибки элементу input
 const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
     inputElement.classList.remove('popup__input_type_error');
     errorElement.classList.remove('popup__input-error_active');
     errorElement.textContent = null;
 };
+
+// Проверка ввода на валидность
 
 const checkInputValidity = (formElement, inputElement) => {
     if (!inputElement.validity.valid) {
@@ -23,13 +28,60 @@ const checkInputValidity = (formElement, inputElement) => {
     }
 };
 
-const setEventListeners = ()
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+    // Если поле не валидно, вернёт true
+    // Обход массива прекратится и вся фунцкция
+    // hasInvalidInput вернёт true
+        return !inputElement.validity.valid;
+    });
+
+};
 
 
-formElement.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-});
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add('popup__submit-button_inactive');
+    } else {
+        buttonElement.classList.remove('popup__submit-button_inactive');
+    }
+}
 
-formInput.addEventListener('input', function () {
-    checkInputValidity();
-});
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__submit-button');
+  
+    // проверка состояния кнопки в самом начале
+    toggleButtonState(inputList, buttonElement);
+
+    inputList.forEach ((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            checkInputValidity(formElement, inputElement);
+            toggleButtonState(inputList, buttonElement);
+        });
+    });
+};
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.popup__form'));
+    formList.forEach ((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement);
+    });
+};
+
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__submit-button',
+    inactiveButtonClass: '.popup__submit-button_inactive',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input-error_active'
+  });
+
